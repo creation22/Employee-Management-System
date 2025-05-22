df321cfd9ad1bf05652c6ff94f116b351ab455c2
creation <creation2224@gmail.com>
2025-05-22 11:22:13 +0530

    addded analytics

--- src/main.jsx
+++ src/main.jsx
@@ -3,10 +3,11 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import AuthProvider from './context/AuthProvider.jsx'
-
+import { Analytics } from "@vercel/analytics/next"
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <AuthProvider>
+      <Analytics />
       <App />
     </AuthProvider>
       
